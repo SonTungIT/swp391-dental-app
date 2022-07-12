1fd97c3ae3ce6d8906e8de7e0a6ca56ec1620a7d
QuangVan2001 <quangvanpham02022001@gmail.com>
2022-07-12 11:20:34 +0700

    chuyển trang giới thiệu và chuyên gia thành trang động

--- src/java/sample/informattion/NewsDAO.java
+++ src/java/sample/informattion/NewsDAO.java
@@ -170,9 +170,108 @@ public class NewsDAO {
         return null;
     }
 
+    
+    public NewsDTO getAbout1() throws SQLException {
+
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            conn = DBUtils.getConnection();
+            if (conn != null) {
+                String sql = "select  top 1 * from News where categoryId = 3 order by timepost desc";
+                stm = conn.prepareStatement(sql);
+                rs = stm.executeQuery();
+                while (rs.next()) {
+                    NewsDTO news = new NewsDTO(rs.getInt(1),
+                            rs.getString(2),
+                            rs.getString(3),
+                            rs.getString(4),
+                            rs.getString(5),
+                            rs.getString(6),
+                            rs.getString(7),
+                            rs.getString(8),
+                            rs.getString(9),
+                            rs.getString(10),
+                            rs.getString(11),
+                            rs.getString(12),
+                            rs.getString(13),
+                            rs.getString(14),
+                            rs.getString(15),
+                            rs.getString(16),
+                            rs.getDate(17),
+                            rs.getInt(18),
+                            rs.getString(19));
+                    return news;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stm != null) {
+                stm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+        return null;
+    }
+    public NewsDTO getExpect1() throws SQLException {
+
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            conn = DBUtils.getConnection();
+            if (conn != null) {
+                String sql = "select  top 1 * from News where categoryId = 4 order by timepost desc";
+                stm = conn.prepareStatement(sql);
+                rs = stm.executeQuery();
+                while (rs.next()) {
+                    NewsDTO news = new NewsDTO(rs.getInt(1),
+                            rs.getString(2),
+                            rs.getString(3),
+                            rs.getString(4),
+                            rs.getString(5),
+                            rs.getString(6),
+                            rs.getString(7),
+                            rs.getString(8),
+                            rs.getString(9),
+                            rs.getString(10),
+                            rs.getString(11),
+                            rs.getString(12),
+                            rs.getString(13),
+                            rs.getString(14),
+                            rs.getString(15),
+                            rs.getString(16),
+                            rs.getDate(17),
+                            rs.getInt(18),
+                            rs.getString(19));
+                    return news;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stm != null) {
+                stm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+        return null;
+    }
     public static void main(String[] args) throws SQLException {
         NewsDAO dao = new NewsDAO();
-        NewsDTO news = dao.getOnePage(0001);
+        NewsDTO news = dao.getExpect1();
         System.out.println(news);
     }
 }
